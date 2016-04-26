7bf07676b671a549ca340cc230c5b23d874a521d
Rahul Arora <rahularora6532@gmail.com>
2016-04-26 20:02:57 +0530

    updation in news feed & mailbox interface done

---
+++ student/communique/app.js
@@ -0,0 +1,36 @@
+/*---------------------------------------
+Directives & Controller of Communique app:
+a. mailbox panel
+b. read mail panel
+c. compose mail panel
+---------------------------------------*/
+
+var app = angular.module('communique', ['cms']);
+
+// Mailbox Panel ------------------------
+app.directive('mailboxPanel', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'mailbox-panel.html'
+	};
+});
+
+// Read Mail Panel ------------------------
+app.directive('readMailPanel', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'read-mail-panel.html'
+	};
+});
+
+// Compose Mail Panel ------------------------
+app.directive('composeMailPanel', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'compose-mail-panel.html'
+	};
+});
+
+// Mailbox Controller
+app.controller('mailboxCtrl', function(){
+});
