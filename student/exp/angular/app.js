d4b17a86990d9718a3124d4f79a4cf874623e519
Rahul Arora <rahularora6532@gmail.com>
2016-05-07 23:12:46 +0530

    newsfeed update. modals added.

---
+++ student/exp/angular/app.js
@@ -0,0 +1,14 @@
+(function(){
+	var app = angular.module('cms', [ ]);
+
+	app.controller('CMSController',function(){
+		alert('Welcome, Gregg!');
+	});
+
+/*	app.directive('appHeader', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'app-header.html'
+		};
+	});*/
+})();
--- student/newsFeed/app.js
+++ student/newsFeed/app.js
@@ -6,7 +6,7 @@ c. Event Panel
 d. Announcement Panel
 ---------------------------------------*/
 
-var app = angular.module('newsfeed', ['cms']);
+var app = angular.module('newsfeed', ['cms', 'ui.bootstrap', 'modalApp']);
 
 // News & Notice Panel ------------------------
 app.directive('newsNoticePanel', function(){
@@ -35,46 +35,61 @@ app.directive('announcementPanel', function(){
 
 // News Controller
 app.controller('newsCtrl', function(){
-	this.headlines = [{
-		time: "12-04-2016 12:05",
+	this.news = [{
 		headline: "News Headline",
-		description: "Description about the attachment can be placed here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Description about the attachment can be placed here.Description about the attachment can be placed here. Description about the attachment can be placed here."
+		description: "Description about the attachment can be placed here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Description about the attachment can be placed here.Description about the attachment can be placed here. Description about the attachment can be placed here.",
+		time: "12-04-2016 12:05",
+		image: "../../assets/dist/img/photo1.png",
+		link: "www.google.com"
 	},
 	{
-		time: "12-04-2016 12:05",
 		headline: "News Headline",
-		description: "Description about the attachment can be placed here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Description about the attachment can be placed here.Description about the attachment can be placed here. Description about the attachment can be placed here."
+		description: "Description about the attachment can be placed here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Description about the attachment can be placed here.Description about the attachment can be placed here. Description about the attachment can be placed here.",
+		time: "12-04-2016 12:05",
+		image: "../../assets/dist/img/photo1.png",
+		link: "www.google.com"
 	},
 	{
-		time: "12-04-2016 12:05",
 		headline: "News Headline",
-		description: "Description about the attachment can be placed here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Description about the attachment can be placed here.Description about the attachment can be placed here. Description about the attachment can be placed here."
+		description: "Description about the attachment can be placed here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Description about the attachment can be placed here.Description about the attachment can be placed here. Description about the attachment can be placed here.",
+		time: "12-04-2016 12:05",
+		image: "../../assets/dist/img/photo1.png",
+		link: "www.google.com"
 	},
 	{
-		time: "12-04-2016 12:05",
 		headline: "News Headline",
-		description: "Description about the attachment can be placed here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Description about the attachment can be placed here.Description about the attachment can be placed here. Description about the attachment can be placed here."
+		description: "Description about the attachment can be placed here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Description about the attachment can be placed here.Description about the attachment can be placed here. Description about the attachment can be placed here.",
+		time: "12-04-2016 12:05",
+		image: "../../assets/dist/img/photo1.png",
+		link: "www.google.com"
 	},
 	{
-		time: "12-04-2016 12:05",
 		headline: "News Headline",
-		description: "Description about the attachment can be placed here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Description about the attachment can be placed here.Description about the attachment can be placed here. Description about the attachment can be placed here."
+		description: "Description about the attachment can be placed here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Description about the attachment can be placed here.Description about the attachment can be placed here. Description about the attachment can be placed here.",
+		time: "12-04-2016 12:05",
+		image: "../../assets/dist/img/photo1.png",
+		link: "www.google.com"
 	}];
+
 	this.notices = [{
-		headline: "Supplementary examination for B.Tech (eve) students / ex - students who are declared failed in the subjects of the odd semester 2015",
-		date: "Dated: 12-04-2016"
+		subject: "Supplementary examination for B.Tech (eve) students / ex - students who are declared failed in the subjects of the odd semester 2015",
+		issueDate: "Dated: 12-04-2016",
+		fileLink: "www.google.com"
 	},
 	{
-		headline: "Supplementary examination for B.Tech (eve) students / ex - students who are declared failed in the subjects of the odd semester 2015",
-		date: "Dated: 12-04-2016"
+		subject: "Supplementary examination for B.Tech (eve) students / ex - students who are declared failed in the subjects of the odd semester 2015",
+		issueDate: "Dated: 12-04-2016",
+		fileLink: "www.google.com"
 	},
 	{
-		headline: "Supplementary examination for B.Tech (eve) students / ex - students who are declared failed in the subjects of the odd semester 2015",
-		date: "Dated: 12-04-2016"
+		subject: "Supplementary examination for B.Tech (eve) students / ex - students who are declared failed in the subjects of the odd semester 2015",
+		issueDate: "Dated: 12-04-2016",
+		fileLink: "www.google.com"
 	},
 	{
-		headline: "Supplementary examination for B.Tech (eve) students / ex - students who are declared failed in the subjects of the odd semester 2015",
-		date: "Dated: 12-04-2016"
+		subject: "Supplementary examination for B.Tech (eve) students / ex - students who are declared failed in the subjects of the odd semester 2015",
+		issueDate: "Dated: 12-04-2016",
+		fileLink: "www.google.com"
 	}];
 });
 
@@ -85,40 +100,75 @@ app.controller('noticeCtrl', function(){
 // Event Controller
 app.controller('eventCtrl', function(){
 	this.events = [{
-		eventDate: "27",
-		eventMonth: "Apr",
-		eventTitle: "Training & Placements",
-		eventVenue: "Senate Hall, Admin Block"
+		eventName: "ICICI Trinity Orientation",
+		description: "Orientation of Trinity challenge 2016 in association with ICICI Orientation of Trinity challenge 2016 in association with ICICIOrientation of Trinity challenge 2016 in association with ICICIOrientation of Trinity challenge 2016 in association with ICICI",
+		image: "../../assets/dist/img/photo1.png",
+		organisedBy: "Society of Software Engineers",
+		startDateTime: {day: "27", month: "May", year: "2016", hour: "11", minute: "30"},
+		endDateTime: {day: "27", month: "May", year: "2016", hour: "21", minute: "30"},
+		website: "http://www.google.com",
+		fbEvent: "http://www.facebook.com",
+		venue: "Senate Hall, Admin Block",
+		mobile: "+91-9999555666",
+		email: "xyz@gmail.com"
 	},
 	{
-		eventDate: "27",
-		eventMonth: "Apr",
-		eventTitle: "Training & Placements",
-		eventVenue: "Senate Hall, Admin Block"
+		eventName: "ICICI Trinity Orientation",
+		description: "Orientation of Trinity challenge 2016 in association with ICICI Orientation of Trinity challenge 2016 in association with ICICIOrientation of Trinity challenge 2016 in association with ICICIOrientation of Trinity challenge 2016 in association with ICICI",
+		image: "../../assets/dist/img/photo1.png",
+		organisedBy: "Society of Software Engineers",
+		startDateTime: {day: "27", month: "May", year: "2016", hour: "11", minute: "30"},
+		endDateTime: {day: "27", month: "May", year: "2016", hour: "21", minute: "30"},
+		website: "http://www.google.com",
+		fbEvent: "http://www.facebook.com",
+		venue: "Senate Hall, Admin Block",
+		mobile: "+91-9999555666",
+		email: "xyz@gmail.com"
 	},
 	{
-		eventDate: "27",
-		eventMonth: "Apr",
-		eventTitle: "Training & Placements",
-		eventVenue: "Senate Hall, Admin Block"
+		eventName: "ICICI Trinity Orientation",
+		description: "Orientation of Trinity challenge 2016 in association with ICICI Orientation of Trinity challenge 2016 in association with ICICIOrientation of Trinity challenge 2016 in association with ICICIOrientation of Trinity challenge 2016 in association with ICICI",
+		image: "../../assets/dist/img/photo1.png",
+		organisedBy: "Society of Software Engineers",
+		startDateTime: {day: "27", month: "May", year: "2016", hour: "11", minute: "30"},
+		endDateTime: {day: "27", month: "May", year: "2016", hour: "21", minute: "30"},
+		website: "http://www.google.com",
+		fbEvent: "http://www.facebook.com",
+		venue: "Senate Hall, Admin Block",
+		mobile: "+91-9999555666",
+		email: "xyz@gmail.com"
 	},
 	{
-		eventDate: "27",
-		eventMonth: "Apr",
-		eventTitle: "Training & Placements",
-		eventVenue: "Senate Hall, Admin Block"
+		eventName: "ICICI Trinity Orientation",
+		description: "Orientation of Trinity challenge 2016 in association with ICICI Orientation of Trinity challenge 2016 in association with ICICIOrientation of Trinity challenge 2016 in association with ICICIOrientation of Trinity challenge 2016 in association with ICICI",
+		image: "../../assets/dist/img/photo1.png",
+		organisedBy: "Society of Software Engineers",
+		startDateTime: {day: "27", month: "May", year: "2016", hour: "11", minute: "30"},
+		endDateTime: {day: "27", month: "May", year: "2016", hour: "21", minute: "30"},
+		website: "http://www.google.com",
+		fbEvent: "http://www.facebook.com",
+		venue: "Senate Hall, Admin Block",
+		mobile: "+91-9999555666",
+		email: "xyz@gmail.com"
 	},
 	{
-		eventDate: "27",
-		eventMonth: "Apr",
-		eventTitle: "Training & Placements",
-		eventVenue: "Senate Hall, Admin Block"
+		eventName: "ICICI Trinity Orientation",
+		description: "Orientation of Trinity challenge 2016 in association with ICICI Orientation of Trinity challenge 2016 in association with ICICIOrientation of Trinity challenge 2016 in association with ICICIOrientation of Trinity challenge 2016 in association with ICICI",
+		image: "../../assets/dist/img/photo1.png",
+		organisedBy: "Society of Software Engineers",
+		startDateTime: {day: "27", month: "May", year: "2016", hour: "11", minute: "30"},
+		endDateTime: {day: "27", month: "May", year: "2016", hour: "21", minute: "30"},
+		website: "http://www.google.com",
+		fbEvent: "http://www.facebook.com",
+		venue: "Senate Hall, Admin Block",
+		mobile: "+91-9999555666",
+		email: "xyz@gmail.com"
 	}];
 });
 
 // Announcement Controller
 app.controller('announcementCtrl', function(){
-	this.timeline = [{
+	this.announcement = [{
 		time: "12:05",
 		heading: "Civil department is holding the practical exams from 14th May",
 		description: "Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem plugg dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle quora plaxo ideeli hulu weebly balihoo...",
