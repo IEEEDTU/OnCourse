b78a0bf26edce62ababeacdf752a5618488af2a8
Rahul Arora <rahularora6532@gmail.com>
2016-05-24 14:22:02 +0530

    profile modals created.

--- student/profiler/modal.js
+++ student/profiler/modal.js
@@ -85,3 +85,50 @@ var editProjectModalInstanceCtrl = function ($scope, $modalInstance, projectForm
         $modalInstance.dismiss('cancel');
     };
 };
+
+
+// For editing about me details -----------------
+app.controller("aboutModalCtrl", ['$scope', '$modal', '$log',
+
+    function ($scope, $modal, $log) {
+
+        $scope.editAboutDialog = function (details){
+            $scope.message = "Show Form Button Clicked";
+            console.log($scope.message);
+            $scope.details = details;
+
+            var modalInstance = $modal.open({
+                templateUrl: 'edit-about-me-modal.html',
+                controller: editAboutModalInstanceCtrl,
+                scope: $scope,
+                resolve: {
+                    aboutForm: function () {
+                        return $scope.aboutForm;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+}]);
+
+var editAboutModalInstanceCtrl = function ($scope, $modalInstance, aboutForm) {
+    $scope.form = {}
+    $scope.submitForm = function () {
+        if ($scope.form.aboutForm.$valid) {
+            console.log('about form is in scope');
+            console.log($scope.form.aboutForm);
+            $modalInstance.close('closed');
+        } else {
+            console.log('aboutForm is not in scope');
+        }
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
